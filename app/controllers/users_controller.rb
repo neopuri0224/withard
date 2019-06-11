c53ca7f72fecd805f3628acf5cc40b0bb22b9fd5
neopuri0224 <neopuri0224@gmil.com>
2019-06-11 11:41:57 +0000

    [Fixed]followers_view/[Add]Scroll top btn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,13 +32,13 @@ class UsersController < ApplicationController
 
   def following
     @user  = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
+    @users = @user.following.page(params[:page])
     render 'show_follow'
   end
 
   def followers
     @user  = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.page(params[:page])
     render 'show_follow'
   end
 
