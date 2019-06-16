76bfae487b7705877a1a42657d7e9f1ce8b479cc
neopuri0224 <neopuri0224@gmil.com>
2019-06-16 12:52:57 +0000

    [Update]js/[Create]search_form

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,4 +49,12 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
+  def self.search(search) #self.でクラスメソッドとしている
+    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      User.where(['name LIKE ?', "%#{search}%"])
+    else
+      User.all #全て表示。
+    end
+  end
+
 end
