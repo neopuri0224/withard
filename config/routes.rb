90cdba4fc0576279cbc26c486a49b20556162d80
neopuri0224 <neopuri0224@gmil.com>
2019-06-18 10:02:03 +0000

    [Create]rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'static_pages#top'
   get 'static_pages/top'
   get 'users/search' => 'users#search'
