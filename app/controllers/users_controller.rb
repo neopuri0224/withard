216c39141d65b400ddfff7f270313fc8e7f1c1d7
neopuri0224 <neopuri0224@gmil.com>
2019-06-20 12:17:55 +0000

    [Fixed]users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,9 @@ class UsersController < ApplicationController
 
   def edit
   	@user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to edit_user_path(current_user)
+    end
   end
 
   def update
