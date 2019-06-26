9e495c0049f9fc1d812439dccc51eafc7c7338d0
neopuri0224 <neopuri0224@gmil.com>
2019-06-26 08:49:19 +0000

    [Fixed]some views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
 
-	def after_sign_in_path_for(*)
+	def after_sign_in_path_for(resource)
 	  posts_path
 	end
 
-	def after_sign_out_path_for(*)
+	def after_sign_out_path_for(resource)
 	  root_path
 	end
 
